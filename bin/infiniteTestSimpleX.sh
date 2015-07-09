09e3e7c023d83f13ead5c6a502e575d5e916ceeb
Jakob Kroeker <kroeker@math.uni-hannover.de>
2015-07-09 11:02:52 +0200

    added minimize support

--- bin/infiniteTestSimpleX.sh
+++ bin/infiniteTestSimpleX.sh
@@ -6,46 +6,82 @@
 # $5 = ignore overflow error
 # $6 = consider timeout
 # $7 = check for out of mem
-# $8 = tmelimit
-# $9 = memory limit
-# ${10} = max num bugs
-# ${11} = idx 
+# $8 = minimize
+# $9 = tmelimit
+# ${10} = memory limit
+# ${11} = max num bugs
+# ${12} = idx 
 # set memlimit e.g.:  ulimit -v 1000000 ; 
 $1;
 runornot=$?
     
 ignoreOverflow=$5
 
-basicTimeout=$8
+basicTimeout=$9
 killTimeout=$(($basicTimeout +30))
-extTimeout=$((10 * $8))
+extTimeout=$((10 * $9))
 extkillTimeout=$(($extTimeout +30))
 
 considerTimeout=$6
 
 checkOutOfMem=$7
 
+minimize=$8
+
+
+maxMinimizeTime=$((3600*5))
+#maxMinimizeTime=1
+
+elapsedMinimizeTime=0
+
+
 echo "binary:   "$2
 echo "testing   "$3
 echo "ignoreOverflow   "$ignoreOverflow
 echo "considerTimeout  "$considerTimeout
 echo "checkOutOfMem    "$checkOutOfMem
+echo "minimize    "$minimize
 
 keepLog=$4
 #echo "keepLog:"$keepLog
 if [ $keepLog -eq 1 ] ; then  echo "keep log " ; fi;
 if [ $keepLog -eq 0 ] ; then  echo " do not keep log " ; fi;
-echo "timeout:  "$8" sec"
-echo "memlimit: "$9" kb"
-echo "maxBugs : "${10}" "
-maxBugs=$((${10} + 0));
+echo "timeout:  "$9" sec"
+echo "memlimit: "${10}" kb"
+echo "maxBugs : "${11}" "
+maxBugs=$((${11} + 0));
 export rnd=$RANDOM
-export idx=${11}
+export idx=${12}
 echo "idx : "$idx" "
 
 
-ulimit -v $9
+ulimit -v ${10}
 export count=0;
+
+#export degSub=0;
+#export termSub=0;
+#export genSub=0;
+#export coeffSub=0;
+#export varSub=0;
+
+declare -A arr
+
+arr["degSub"]=0
+arr["termSub"]=0
+arr["genSub"]=0
+arr["coeffSub"]=0
+arr["varSub"]=0
+
+
+declare -A arrProc
+
+arrProc["degSub"]="minimizeMonomialDegS"
+arrProc["termSub"]="minimizeTermNumberS"
+arrProc["genSub"]="minimizeGensS"
+arrProc["coeffSub"]="minimizeAbsMaxCoeffS"
+arrProc["varSub"]="minimizeVarsS"
+
+export randomMinimizeNum=-1
 
 
 filename="${3##*/}"
@@ -75,6 +111,9 @@ then
     . log/$filename/bugs/id_$idx.count
 fi
 
+for key in ${!arr[@]}; do
+    echo ${key} ${arr[${key}]}
+done
 
 export hid=`hostname |sed "s/[^0-9]//g"`
 
@@ -106,38 +145,100 @@ do
       sleep 300;
       continue;
     fi;
+
     export randomNum=$(od -N 6 -t uL -An /dev/urandom | tr -d " ") 
     export randomNum=$( echo $randomNum" % 2^30" | bc ) # have to cut random num (on 32 bit machine only?)
-    echo $randomNum
+
+
+    if [ $minimize -eq 1 ]
+    then
+       echo "minimize"
+       if [ $elapsedMinimizeTime -gt $maxMinimizeTime ]
+       then
+            export minPos=0;
+            if [ $randomMinimizeNum -eq -1 ]
+            then
+              export randomMinimizeNum=$( echo $randomNum" % "${#arr[@]} | bc ) 
+            fi;
+            for key in ${!arr[@]}; do
+                if [ $minPos -eq $randomMinimizeNum ]
+                then 
+                    arr[${key}]=$((${arr[${key}]}-1))   
+                fi; 
+                export minPos=$(($minPos + 1)); 
+            done;
+         
+       fi;
+    fi;
+
+
+
+    echo "randomNum: " $randomNum
+
     rm -f log/$filename/$filename.id_$idx.$count
     sleep 3
     echo "count: "$count;
     #echo 'def countstr="'$count'";' >input/$3.count; 
     touch log/$filename/id_$idx.$count
-    echo 'string logfile = "log/'$filename'/id_'$idx'.'$count'";'  >/tmp/testsingular/input/$filename/$filename.in; 
+    echo 'string logfile = "log/'$filename'/id_'$idx'.'$count'";'  >/tmp/testsingular/input/$filename/$filename.$idx.in; 
 
-    cat $fullinput >> /tmp/testsingular/input/$filename/$filename.in; 
+    cat $fullinput >> /tmp/testsingular/input/$filename/$filename.$idx.in;
+
+    for key in ${!arr[@]}; do
+
+       echo ${arrProc[${key}]}"("${arr[${key}]}");" >> /tmp/testsingular/input/$filename/$filename.$idx.in;
+   done
+
+   cat /tmp/testsingular/input/$filename/$filename.$idx.in;    
+
+    echo " if (defined(run)) { run(); }" >> /tmp/testsingular/input/$filename/$filename.$idx.in;  
 
     if [ $keepLog -eq 1 ] 
     then
-        #set -o pipefail; timeout -s SIGKILL $basicTimeout $2 -v -r $randomNum /tmp/testsingular/input/$filename/$filename.in 2>&1 | tee -a log/$filename/id_$idx.log;
-        set -o pipefail; timeout --kill-after=5 $basicTimeout  $2  -v -r $randomNum /tmp/testsingular/input/$filename/$filename.in 2>&1 | tee -a log/$filename/id_$idx.log;
+        #set -o pipefail; timeout -s SIGKILL $basicTimeout $2 -v -r $randomNum /tmp/testsingular/input/$filename/$filename.$idx.in 2>&1 | tee -a log/$filename/id_$idx.log;
+        set -o pipefail; timeout --kill-after=5 $basicTimeout  $2  -v -r $randomNum /tmp/testsingular/input/$filename/$filename.$idx.in 2>&1 | tee -a log/$filename/id_$idx.log;
         status=$?; echo "status="$status;echo "status"$status >> log/$filename/id_$idx.log;
     else
-        set -o pipefail; timeout --kill-after=5 $basicTimeout  $2 -v -r $randomNum /tmp/testsingular/input/$filename/$filename.in 2>&1 | tee log/$filename/id_$idx.log;
+        set -o pipefail; timeout --kill-after=5 $basicTimeout  $2 -v -r $randomNum /tmp/testsingular/input/$filename/$filename.$idx.in 2>&1 | tee log/$filename/id_$idx.log;
         status=$?; echo "status="$status;echo "status"$status >> log/$filename/id_$idx.log;
     fi;   
-    echo "show output file" 
+    echo "show output file ******************************************" 
     cat log/$filename/id_$idx.$count;
+    cat log/$filename/id_$idx.$count > /tmp/testsingular/input/$filename/$filename.$idx.output;
     sync
     #echo 3 > /proc/sys/vm/drop_caches
-    echo "show output file after sync" 
+    echo "show output file after sync+++++++++++++++++++++++++++++++++++"
     cat log/$filename/id_$idx.$count;
 
-    sleep 5; # wait for disc sync...
-    echo "show output file after sleep" 
+    # sleep 15; # wait for disc sync...
+    echo "show output file after sleep ----------------------------------" 
     cat log/$filename/id_$idx.$count;
-    sleep 3;  
+    # sleep 20;  
+    diff log/$filename/id_$idx.$count /tmp/testsingular/input/$filename/$filename.$idx.output;
+    diffstatus=$?;
+    if [ $diffstatus -ne 0 ]
+    then 
+         cp /tmp/testsingular/input/$filename/$filename.$idx.output log/$filename/bugs/id_$idx.$count.lag.bug;
+         cp log/$filename/id_$idx.$count log/$filename/bugs/id_$idx.$count.changedout.bug;
+
+         # duplicate code !!
+         if [ -e "log/$filename/id_$idx.$count" ]
+         then 
+             mv log/$filename/id_$idx.$count log/$filename/bugs/id_$idx.$count.bug;
+         fi;
+
+         export count=$(($count + 1)); 
+         echo "#!/bin/bash">  log/$filename/bugs/id_$idx.count.tmp
+         echo " export count="$count >>  log/$filename/bugs/id_$idx.count.tmp
+          for key in ${!arr[@]}; do
+             echo "arr[\""${key}"\"]="${arr[${key}]} >>log/$filename/bugs/id_$idx.count.tmp
+          done
+
+         mv log/$filename/bugs/id_$idx.count.tmp log/$filename/bugs/id_$idx.count
+         continue;
+    fi;
+    elapsedMinimizeTime=$((${elapsedMinimizeTime} + $basicTimeout));
+
     if [ $status -eq 253 ] || [ $status -eq 252 ]
     then
        overflow=1;
@@ -159,6 +260,11 @@ do
         # export count=$(($count )); 
         echo "doNothing ";echo "doNothing ">> log/$filename/id_$idx.log;
     else 
+
+        export randomMinimizeNum=$( echo $randomNum" % "${#arr[@]} | bc ) 
+
+        echo "randomMinimizeNum: " $randomMinimizeNum
+
         echo "raiseCount ";echo "raiseCount ">> log/$filename/id_$idx.log;
         echo "quit;" >> log/$filename/id_$idx.$count;
 
@@ -186,11 +292,28 @@ do
         export count=$(($count + 1)); 
         echo "#!/bin/bash">  log/$filename/bugs/id_$idx.count.tmp
         echo " export count="$count >>  log/$filename/bugs/id_$idx.count.tmp
+
+
+        if [ $minimize -eq 1 ]
+        then 
+            export minPos=0;
+
+            for key in ${!arr[@]}; do
+                if [ $minPos -eq $randomMinimizeNum ]
+                then 
+                    arr[${key}]=$((${arr[${key}]}+1))   
+                fi;
+                echo "arr[\""${key}"\"]="${arr[${key}]} >>log/$filename/bugs/id_$idx.count.tmp
+                export minPos=$(($minPos + 1)); 
+            done;
+
+        fi;
+
         mv log/$filename/bugs/id_$idx.count.tmp log/$filename/bugs/id_$idx.count
     fi; 
 done
 echo "done"
-echo $1" "$2" "$3" "$4" "$5" "$6" "$8" "$9" "${10}" "${11}
+echo $1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}
 #timeout:     124
 #out of mem:  14
 
